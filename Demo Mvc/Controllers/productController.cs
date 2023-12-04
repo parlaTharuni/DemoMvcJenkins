20f2ba7a958a6058363f2bca9491688d7ac8230f
parlaTharuni <tharuniparla@gmail.com>
2023-12-04 17:35:46 +0530

    Add project files.

---
+++ Demo Mvc/Controllers/productController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo_Mvc;
+using Demo_Mvc.Models;
+
+namespace Demo_Mvc.Controllers
+{
+    public class productController : Controller
+    {
+        // GET: product
+        public ActionResult Index()
+        {
+           // return View();
+            productsmodel pro = new productsmodel();
+            pro.prodid = 1;
+            pro.prodname = "abc";
+                return View();
+        }
+    }
+}
---
+++ Demo Mvc/Models/productsmodel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_Mvc.Models
+{
+    public class productsmodel
+    {
+        public int prodid { get; set; }
+
+    }
+}
